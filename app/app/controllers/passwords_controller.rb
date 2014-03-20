1955e2e080e4f11c908dfac309d75693fc76fc54
vergeman <alan.verga@gmail.com>
2014-03-19 23:05:58 -0400

    removed extraneous stub code

--- app/app/controllers/passwords_controller.rb
+++ app/app/controllers/passwords_controller.rb
@@ -2,33 +2,6 @@ class PasswordsController < Devise::PasswordsController
   include ApplicationHelper
   respond_to :html, :json
 
-  # GET /resource/password/edit?reset_password_token=abcdef
-  def edit
-    self.resource = resource_class.new
-    resource.reset_password_token = params[:reset_password_token]
-    puts "===="
-    puts resource.errors.inspect
-  end
-
-
- # PUT /resource/password
-  def update
-    self.resource = resource_class.reset_password_by_token(resource_params)
-    yield resource if block_given?
-
-    if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-      set_flash_message(:notice, flash_message) if is_flashing_format?
-      sign_in(resource_name, resource)
-      respond_with resource, location: after_resetting_password_path_for(resource)
-    else
-      puts "===="
-      puts resource.errors.inspect
-
-      respond_with resource
-    end
-  end
 
   # POST /resource/password
   def create
