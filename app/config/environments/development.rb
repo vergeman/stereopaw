ade9954768779170c575a4e24d98b12802af1233
vergeman <alan.verga@gmail.com>
2014-03-19 22:25:11 -0400

    Mandrill mailer addition

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -27,5 +27,16 @@ App::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'stereopaw.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV['STEREOPAW_MANDRILL_USERNAME'],
+    :password  => ENV['STEREOPAW_MANDRILL_APIKEY_TEST'], # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'stereopaw.com', # your domain to identify your server when connecting
+  }
+
 end
--- app/config/environments/production.rb
+++ app/config/environments/production.rb
@@ -77,4 +77,19 @@ App::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #mandrill
+  config.action_mailer.default_url_options = { :host => 'stereopaw.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV['STEREOPAW_MANDRILL_USERNAME'],
+    :password  => ENV['STEREOPAW_MANDRILL_APIKEY'], # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'stereopaw.com', # your domain to identify your server when connecting
+  }
+
+
 end
