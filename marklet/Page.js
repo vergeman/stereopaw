43c3ccb0226b8e2951ada0feb9b518410480f1c3
vergeman <alan.verga@gmail.com>
2014-04-02 11:47:01 -0400

    marklet css 'objectified' and parse function

--- marklet/Page.js
+++ marklet/Page.js
@@ -50,65 +50,116 @@ SB.Page = (function() {
 
 	    '</div>'; //player,
 	    
-	var _style =
-	'#sb-app { ' + 
-	    'position: fixed; ' +
-	    'top: 50px; ' +
-	    'right: 0; ' +
-	    'width: 300px;' +
-	    'background-color: #fff;' +
-	    'border: 1px solid #aaa;' +
-	    'z-index: 99999999999999999;' +
-	    'text-align:left;' +
-	    'display:none' +
-	    '}' +
+    var _styles = {
+	'#sb-app' : {
+	    'position' : "fixed",
+	    'top' : "50px",
+	    'right' : "0",
+	    'width' : "300px",
+	    'background-color' : "#fff",
+	    'border' : "1px solid #aaa",
+	    'z-index': "99999999999999999",
+	    'text-align' : "left",
+	    'display' : "none"
+	},
 
-    '#sb-title { padding: 10px; margin-top: 8px; border-bottom: 1px solid #000; margin-left: 10px; margin-right: 10px;} ' +
+	'#sb-title' : { 
+	    'padding': "10px",
+	    'margin-top': "8px",
+	    'border-bottom' : "1px solid #000",
+	    'margin-left' : "10px",
+	    'margin-right': "10px"
+	},
 
-'#sb-track { margin-left: 10px; font-size: 14px; padding-top: 10px; margin-right: 10px; font: 12px/1.4 "Lucida Grande","Lucida Sans Unicode","Lucida Sans",Garuda,Verdana,Tahoma,sans-serif; }' +
+	'#sb-track' : { 
+	    'margin-left' : "10px",
+	    'font-size' : "14px",
+	    'padding-top': "10px",
+	    'margin-right' : "10px",
+	    'font': '12px/1.4 "Lucida Grande","Lucida Sans Unicode","Lucida Sans",Garuda,Verdana,Tahoma,sans-serif;'
+	},
+
+	'#sb-close' : {
+	    'float' : "right",
+	    'padding': "8px",
+	    'padding-right' : "6px",
+	    'cursor': "pointer"
+	},
+
+	'#sb-logo' : { 
+	    'float' : "left",
+	    'padding' : "8px",
+	    'padding-left' : "10px",
+	    'margin-right' : "4px"
+	},
 	
+	'#sb-time' : { 
+	    'font-size' : "14px"
+	},
 
-	'#sb-close { ' +
-	    'float:right;padding: 8px; padding-right: 6px; cursor: pointer;' +
-	    '}' +
+	'#sb-display-bar' : { 
+	    'width' : "100%",
+	    'cursor' : "pointer", 
+	    'height' : "10px",
+	    'background-color' : "#000",
+	    'display' : "none"
+	},
 
-'#sb-logo { float:left; padding: 8px; padding-left: 10px; margin-right: 4px;} ' +
+	'#sb-display-bar-elapsed' : {
+	    'width' : "0%", 
+	    'height' : "8px",
+	    'background-color' : "#aaa"
+	},
 
-'#sb-time { font-size: 14px; }'+
+	'.sb-label' : { 
+	    'font-size' : "12px",
+	    'margin-top' : "10px", 
+	    'color': "#a2a2a2"
+	},
 
+	'#sb-track-title' : { 'font-size' : "18px" },
+	'#sb-track-artist' : { 'font-size' : "14px" },
+	//'#sb-track-service { font-size: 12px; margin-top: 10px;}' +
 
-	'#sb-display-bar { ' +
-	    'width: 100%;' +
-	    'cursor: pointer;' + 
-	    'height: 10px;' + 
-	    'background-color: #000;' +
-	'display:none;' +
-	    '}' +
+	'#sb-display-seek' : {
+	    'position' : "relative",
+	    'top' : "-8px",
+	    'width' : "2px", 
+	    'height' : "10px",
+	    'background-color' : "#eee111",
+	    'display' : "none"
+	},
 
-	'#sb-display-bar-elapsed { ' +
-	    'width: 0%;' +
-	    'height: 8px;' +
-	    'background-color: #aaa' +
-	'}' +
+	'#sb-submit' : { 
+	    'width' : "100%;",
+	    'margin-top' : "20px"
+	},
 
-'.sb-label { font-size: 12px; margin-top: 10px; color: #a2a2a2;}' +
-'#sb-track-title { font-size: 18px;}' +
-'#sb-track-artist { font-size: 14px; }' +
-//'#sb-track-service { font-size: 12px; margin-top: 10px;}' +
+	'#sb-submit-button' : { 
+	    'background': "#0066cc",
+	    'color' : "white",
+	    'padding': "10px",
+	    'margin': "10px",
+	    'text-align' : "center",
+	    'font-size': "14px",
+	    'cursor' : "pointer"
+	}
 
-	'#sb-display-seek { ' +
-	    'position:relative;' +
-	    'top: -8px;' +
-	    'width: 2px;' +
-	    'height: 10px;' +
-	    'background-color: #eee111;' +
-	    'display:none;' +	
-	'}' +
+    };
 
-'#sb-submit { width: 100%; margin-top: 20px;}' +
+    function parseCSSObj(obj) {
+	css = ""
+	for (key in obj) {
+	    if (obj.hasOwnProperty(key) && typeof obj[key] == "object") {
+		css += key + "{" + parseCSSObj(obj[key]) + "}"
+	    }
+	    if (obj.hasOwnProperty(key) && typeof obj[key] == "string") {
+		css += key + ":" + obj[key] + ";"
+	    }
+	}
+	return css
+    };
 
-    '#sb-submit-button { background: #0066cc;  color:white; padding: 10px; margin: 10px; text-align: center; font-size: 14px; cursor:pointer;}';
-    
     function build_element(element, content) {
 	var e = document.createElement(element.type)
 	e.setAttribute(element.attr, element.value)
@@ -135,7 +186,7 @@ SB.Page = (function() {
 		    attr : "id",
 		    value : "sb-style"
 		},
-		_style
+		parseCSSObj(_styles)
 	    )
 
 	}
