1c4c6a76b5c1b53c8c172e77d8aeba0a23556b4a
vergeman <alan.verga@gmail.com>
2014-04-28 05:23:01 -0400

    config chng

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -13,7 +13,7 @@ App::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
   config.cache_store = :memory_store
-  config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public"
+  #config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public"
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
